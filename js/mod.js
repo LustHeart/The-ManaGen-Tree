0b21c5d84c2e7aebaf5b19771103ddb237b905f9
LustHeart <126852616+LustHeart@users.noreply.github.com>
2023-11-19 00:21:44 +0700

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -20,7 +20,7 @@ let VERSION = {
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.1</h3><br>
 		- Added Mana Upgrade<br>
-		- [New Prestige Layer] Orb
+		- [New Prestige Layer] Orb`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 
